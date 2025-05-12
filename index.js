52237d13204dd01a3897b687c36d9096c4e35102
Leo <leonardoloayza28@gmail.com>
2025-05-11 22:59:07 -0500

    fixing

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(session({
 // Conexión a MySQL
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_NAME,      
+  user: process.env.DB_USERNAME,      
   password: process.env.DB_PASSWORD, 
-  database: process.env.DB_USERNAME  
+  database: process.env.DB_NAME  
 });
 
 db.connect((err) => {
