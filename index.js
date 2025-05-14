270522387402fc7cf3e0aaced1de29759e002c03
Leo <leonardoloayza28@gmail.com>
2025-05-14 00:01:12 -0500

    allowing request from every locationAAAA

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const mysql = require('mysql2');
 const session = require('express-session');
+const cors = require('cors'); 
 
 const app = express(); 
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+  credentials: true 
+}));
 
 // Middlewares necesarios
 app.use(express.json());
